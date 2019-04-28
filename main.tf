cb2a75f255a4115736082f3874dde6c24c6ecc5e
unknown <dermot@singlepane.com>
2019-04-28 13:04:31 +0100

    initial

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "my_resource_group" {
-  source = "../modules/resource_group"
+  source = "./resource_group"
   resource_group_name = "TESTING"
   resource_group_location = "West US"
   resource_group_environment = "dev"
